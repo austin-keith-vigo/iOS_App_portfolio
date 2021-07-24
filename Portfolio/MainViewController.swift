1534d73ad84c9cdaa198aafbfe992bc86ce8c430
austin-keith-vigo <austin.keith.vigo@gmail.com>
2021-07-24 15:01:01 -0700

    Init
    Initialized repo with project

---
+++ Portfolio/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  Portfolio
+//
+//  Created by austin vigo on 7/22/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.initViews()
+    }
+
+    func initViews() {
+        self.view.backgroundColor = .purple
+    }
+
+}
